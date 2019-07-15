72800d045a78ba114d72948237aa823d2131782d
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2019-07-15 17:01:51 +0800

    added comments an docblocks

--- main.go
+++ main.go
@@ -1,3 +1,10 @@
+/*
+	Googol API Framework
+	A minimalist web API framework using the service-go-pattern
+
+	@Author: Karl Anthony B. Baluyot
+*/
+
 package main
 
 import (
@@ -5,6 +12,7 @@ import (
 	"net/http"
 )
 
+//* entry point of the googol framework
 func main() {
 
 	err := http.ListenAndServe(":8080", ChiRouter().InitRouter())
