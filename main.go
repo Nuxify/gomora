f0fbf838b8bbf166245f73569861991482bade57
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2019-07-15 18:00:14 +0800

    added dotenv autoload

--- main.go
+++ main.go
@@ -8,14 +8,24 @@
 package main
 
 import (
+	"fmt"
+	_ "github.com/joho/godotenv/autoload"
 	"log"
 	"net/http"
+	"os"
 )
 
 //* entry point of the googol framework
 func main() {
 
-	err := http.ListenAndServe(":8080", ChiRouter().InitRouter())
+	//get the API_URL_PORT
+	port := os.Getenv("API_URL_PORT")
+	if len(port) == 0 {
+		port = "8000" //! default port 8000
+	}
+
+	//listen for requests and serve
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), ChiRouter().InitRouter())
 	if err != nil {
 		log.Println("Listen and Serve:", err)
 	}
