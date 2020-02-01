542453bf0a7ccd0915ec58d267b33681419401c3
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2020-02-01 14:27:43 +0800

    fix documentation

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 /*
-	The Gomora API Framework
-	A minimalist web API framework using the service-go-pattern
-
-	@Author: Karl Anthony B. Baluyot
+|--------------------------------------------------------------------------
+| Main
+|--------------------------------------------------------------------------
+|
+| This is the entry point for listeners of the project.
+| You can create and run goroutines for event listeners below before the HTTP listener.
+|
 */
-
 package main
 
 import (
@@ -15,16 +17,15 @@ import (
 	"os"
 )
 
-//entry point of the gomora framework
+// entry point of the gomora framework
 func main() {
-
-	//get the API_URL_PORT
+	// get the API_URL_PORT
 	port := os.Getenv("API_URL_PORT")
 	if len(port) == 0 {
-		port = "8000" //! default port 8000
+		port = "8000" // default port 8000
 	}
 
-	//listen for requests and serve
+	// listen for requests and serve
 	err := http.ListenAndServe(fmt.Sprintf(":%s", port), ChiRouter().InitRouter())
 	if err != nil {
 		log.Println("Listen and Serve:", err)
