7a6ea576eaf32e2a3c3099ebfe934880fcf341db
Andrea Ella Remoreras <ae.remoreras@gmail.com>
2025-02-05 17:44:18 +0800

    feat: add new method in mysql db handler

--- infrastructures/database/mysql/types/MySQLDBHandlerInterface.go
+++ infrastructures/database/mysql/types/MySQLDBHandlerInterface.go
@@ -2,11 +2,18 @@ package types
 
 import (
 	"database/sql"
+
+	"github.com/jmoiron/sqlx"
 )
 
 // MySQLDBHandlerInterface contains the implementable methods for the MySQL DB handler
 type MySQLDBHandlerInterface interface {
+	// Begin starts a new transaction
+	Begin() (*sqlx.Tx, error)
+	// Execute executes the mysql statement following NamedExec
 	Execute(stmt string, model interface{}) (sql.Result, error)
+	// Query selects rows given by the sql statement
 	Query(qstmt string, model interface{}, bindModel interface{}) error
+	// QueryRow selects a row given by the sql statement
 	QueryRow(qstmt string, model interface{}, bindModel interface{}) error
 }
