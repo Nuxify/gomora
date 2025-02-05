4b526c20ad0c5f166abcad23c2a06ad2bbf6c974
Andrea Ella Remoreras <ae.remoreras@gmail.com>
2025-02-05 17:49:21 +0800

    fix: add connect and connectviassh methods in interface

--- infrastructures/database/mysql/types/MySQLDBHandlerInterface.go
+++ infrastructures/database/mysql/types/MySQLDBHandlerInterface.go
@@ -10,6 +10,10 @@ import (
 type MySQLDBHandlerInterface interface {
 	// Begin starts a new transaction
 	Begin() (*sqlx.Tx, error)
+	// Connect opens a new connection to the mysql interface
+	Connect(params ConnectionParams) error
+	// ConnectViaSSH opens a new connection to the mysql interface via ssh
+	ConnectViaSSH(paramsSSH SSHConnectionParams, params ConnectionParams) error
 	// Execute executes the mysql statement following NamedExec
 	Execute(stmt string, model interface{}) (sql.Result, error)
 	// Query selects rows given by the sql statement
