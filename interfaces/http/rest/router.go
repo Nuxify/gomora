6094a9f692e5d104f590eaf5317629bbd6e1f842
Andrea Ella Remoreras <ae.remoreras@gmail.com>
2025-08-28 13:36:12 +0800

    build: bump api version to 1.10.2

--- interfaces/http/rest/router.go
+++ interfaces/http/rest/router.go
@@ -63,7 +63,7 @@ func (router *router) InitRouter() *chi.Mux {
 			Status:  http.StatusOK,
 			Success: true,
 			Message: "alive",
-			Data:    map[string]string{"version": "v1.10.1"},
+			Data:    map[string]string{"version": "v1.10.2"},
 		}
 
 		response.JSON(w)
